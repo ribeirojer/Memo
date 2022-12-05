34403ef1f400c9f01f8f9f6057c7d8c443f5efcc
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2022-12-04 22:10:42 -0300

    create FeedWidget.tsx

---
+++ src/components/FeedWidget.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const FeedWidget = (props: Props) => {
+  return (
+    <div>
+        <h1>Dê seu feedback</h1>
+        <p>digite aqui</p>
+    </div>
+  )
+}
+
+export default FeedWidget;
